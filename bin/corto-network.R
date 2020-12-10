21d56c4d43a3a30c7210aa3162b079f0e89c1e0b
hachepunto <hatovar@inmegen.gob.mx>
2020-12-10 03:04:26 -0600

    correct saving command

--- bin/corto-network.R
+++ bin/corto-network.R
@@ -10,11 +10,11 @@ parser$add_argument("-l", "--list", default = FALSE,
     help = "Transcription factor list")
 parser$add_argument("-b", "--bootstraps", type = "integer", default = 10,
     help = "Number of bootstraps (default 10)")
-parser$add_argument("-p", "--pvalue", default = 1e-7,
+parser$add_argument("-p", "--pvalue", type = "character", default = 1e-7,
     help = "Treshold p-value (default 1e-7)")
 parser$add_argument("-t", "--threads", type = "integer", default = 2,
     help = "Number of threads (default 2)")
-parser$add_argument("-o", "--out", type = "character", default = "no_out",
+parser$add_argument("-o", "--out", type = "character", default = FALSE,
     help = "Output .RData")
 args <- parser$parse_args()
 
@@ -26,16 +26,9 @@ centroids <- tfs[tfs %in% rownames(inmat)]
 regulon <- corto(inmat,
     centroids = centroids,
     nbootstraps = args$bootstraps,
-    p = args$pvalue,
-    nthreads = args$threads)
+    p = as.numeric(args$pvalue),
+    nthreads = args$threads,
+    verbose = TRUE)
 
-if (args$out == "no_out") {
-        out <- paste0("results/",
-        gsub(".RDS", ".regulon.RData",
-        basename(args$input)
-        )
-    )
-} else {
-        out <- args$out
-        }
-save(regulon, file = out)
+print("Saving")
+save(regulon, file = args$out)
--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ TFS='/labs/usb/Fanconi_SC-RNAseq/data/HsTF.txt'
 # Number of bootstraps
 BOOTSTRAPS=100
 # P value threshold
-PVALUE=1e-7
+PVALUE="1e-7"
 # Number of threads
 THREADS=10
